75f58f97c8f9e6a4fb3749fe37f958f986893707
HibernalGlow <3090253564@qq.com>
2026-06-14 20:17:39 +0800

    chore(version): 更新所有组件版本号至 1.2.9
    - 更新 Go 代码中的版本常量
    - 修改 pyproject.toml 中的项目版本
    - 调整 Windows 构建信息中的文件和产品版本
    - 更新 Python 常量文件中的版本号
    - 修改前端 package.json 中的版本号
    - 更新 Rust 绑定 Cargo.toml 的版本号

--- constants.go
+++ constants.go
@@ -6,7 +6,7 @@ import (
 	"runtime"
 )
 
-const Version = "1.2.8"
+const Version = "1.2.9"
 const UpdateCheckerURL = "https://codepoems.eu/downloads/xl-converter/version.json"
 const UpdateCheckerEnabled = true
 
